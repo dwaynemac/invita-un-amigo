5effb3bcaca32a667dc63da25d4fee48ded4718a
Dwayne Macgowan <dwaynemac@gmail.com>
2015-08-30 16:27:49 -0300

    find in padma on new window

--- app/helpers/invitations_helper.rb
+++ app/helpers/invitations_helper.rb
@@ -1,7 +1,7 @@
 module InvitationsHelper
 
   def find_in_padma(text)
-    link_to text, "http://crm.padm.am/contacts?q=#{text}"
+    link_to text, "http://crm.padm.am/contacts?q=#{text}", target: '_blank'
   end
 
 end
